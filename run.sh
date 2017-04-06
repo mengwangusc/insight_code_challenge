6f3b0d5939c6b75bcab5a15e27b5d0289c4a90eb
Meng Wang <wangmeng@usc.edu>
2017-04-05 17:59:24 -0700

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# one example of run.sh script for implementing the features using python
+# the contents of this script could be replaced with similar files from any major language
+
+# I'll execute my programs, with the input directory log_input and output the files in the directory log_output
+cd src
+javac codeChallenge.java
+java codeChallenge
